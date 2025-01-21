eb20e74e65af64cb22928943694af5c33bde9860
Matheus <matheus.berbet001@umb.edu>
2025-01-21 02:03:39 -0500

    final version for now

---
+++ helmet.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth/3, window.innerHeight/3);
+    
+    const container = document.getElementById('hero-canvas');
+    // append in the front of the container rather than the back
+    container.insertBefore(renderer.domElement, container.firstChild);
+
+    const light = new THREE.DirectionalLight(0xD3B31F, 3);
+    light.position.set(5, 5, 5).normalize();
+    scene.add(light);
+
+    const light2 = new THREE.DirectionalLight(0xD3B31F, 2);
+    light.position.set(-5, -5, 5).normalize();
+    scene.add(light2);
+
+
+    const ambientLight = new THREE.AmbientLight(0x404040, 0.5);
+    scene.add(ambientLight);
+
+    const loader = new THREE.GLTFLoader();
+    var model;
+    loader.load('assets/helmet.glb', (gltf) => {
+        model = gltf.scene;
+        model.scale.set(1, 1, 1);
+        scene.add(model);
+    });
+
+   
+
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    let alt = false;
+    let once = false;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        if (model && !once) {
+            camera.position.set(model.position.x + 1, model.position.y + 0.3, model.position.z + 0.1);
+            once = true;
+        }
+
+        if (model){ 
+            if (alt) model.rotation.y += 0.005;
+            if (!alt) model.rotation.y -= 0.005;
+            if (model.rotation.y >= 0) alt = false;
+            else if (model && model.rotation.y <= -2) alt = true;
+        }
+
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate();
+};
+
---
+++ services/script.js
@@ -0,0 +1,62 @@
+
+const images = [
+    ['/assets/finish_carpentry.png', 'Finish Carpentry', 'Our finish carpentry services include crown molding, baseboards, wainscoting, and more.'],
+    ['/assets/cabinetry.jpg', 'Cabinetry', 'We sell and install prefab cabinets for kitchens, bathrooms, and offices.'],
+    ['/assets/home_improvement.jpg', 'Home Improvement', 'We offer a wide range of home improvement services'],
+    ['/assets/custom_furniture.png', 'Custom Furniture', 'We design and build custom furniture to fit your unique style and needs. Such as this unique Ox Cart Table.'],
+    ['/assets/built_in.png', 'Built-Ins', 'We design and build custom built-ins for your home such as bookcases and more.'],
+    ['/assets/construction.jpg', 'Residential Construction', 'We offer residential construction services, including new home construction, additions, and more.']
+  ];
+  let currentIndex = 0;
+
+  function openModal(index) {
+    const modal = document.getElementById('modal');
+    const modalImg = document.getElementById('modal-img');
+    const modalTitle = document.getElementById('modal-title');
+    const modalDescription = document.getElementById('modal-description');
+
+    currentIndex = index;
+    modal.style.display = 'flex';
+    modal.style.flexDirection = 'column';
+
+    modalImg.src = images[currentIndex][0];
+    modalTitle.textContent = images[currentIndex][1];
+    modalDescription.textContent = images[currentIndex][2];
+
+    if (currentIndex == 1) {
+      // append a button to the modal that says "View Cabinets" and links to the cabinets page
+      modalDescription.insertAdjacentHTML('afterend', '<a href="https://www.pac-kb.com" class="modal-button">View Cabinets</a>');
+    } else {
+      const modalButton = document.querySelector('.modal-button');
+      if (modalButton) {
+        modalButton.remove();
+      }
+    }
+
+  }
+
+  function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+
+  function changeImage(direction) {
+    currentIndex = (currentIndex + direction + images.length) % images.length;
+    const modalImg = document.getElementById('modal-img');
+    const modalTitle = document.getElementById('modal-title');
+    const modalDescription = document.getElementById('modal-description');
+
+    modalImg.src = images[currentIndex][0];
+    modalTitle.textContent = images[currentIndex][1];
+    modalDescription.textContent = images[currentIndex][2];
+
+    if (currentIndex == 1) {
+      // append a button to the modal that says "View Cabinets" and links to the cabinets page
+      modalDescription.insertAdjacentHTML('afterend', '<a href="https://www.pac-kb.com" class="modal-button">View Cabinets</a>');
+    } else {
+      const modalButton = document.querySelector('.modal-button');
+      if (modalButton) {
+        modalButton.remove();
+      }
+    }
+  }
